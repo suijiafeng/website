1c37de503930122a1218d5e0bf7ab212bffc2f17
suijiafeng <suijiafeng@users.noreply.github.com>
2026-05-31 00:40:35 +0000

    deploy: suijiafeng/blog-source@160c5bfea622448af52cdc6b6025ef577470bd14

---
+++ _next/static/-XVa-dIwgjFUd11PCSdgB/_buildManifest.js
@@ -0,0 +1,98 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0c07x-d0a2zl7.js"
+  ],
+  "/404": [
+    "static/chunks/0bfayox~8nn4q.js"
+  ],
+  "/_error": [
+    "static/chunks/06grekr0kswj~.js"
+  ],
+  "/about": [
+    "static/chunks/0jxb_69.8gozh.js"
+  ],
+  "/archive": [
+    "static/chunks/0obay4whhhziw.js"
+  ],
+  "/archive/[year]": [
+    "static/chunks/00i-xxt4~6cbu.js"
+  ],
+  "/blog": [
+    "static/chunks/05ndi0x7qo354.js"
+  ],
+  "/blog/[...slug]": [
+    "static/chunks/140wbsvtmnmhl.js"
+  ],
+  "/blog/about": [
+    "static/chunks/0vuv322jl6d~a.js"
+  ],
+  "/blog/archive": [
+    "static/chunks/02a42.koz_xly.js"
+  ],
+  "/blog/archive/[year]": [
+    "static/chunks/09x7vl942f31r.js"
+  ],
+  "/blog/articles": [
+    "static/chunks/0gxc6vb.o0rfs.js"
+  ],
+  "/blog/articles/[...slug]": [
+    "static/chunks/0o0wr-oownc~q.js"
+  ],
+  "/blog/posts": [
+    "static/chunks/10hz-j~lubi0p.js"
+  ],
+  "/blog/search": [
+    "static/chunks/00_yw8cu.3gfz.js"
+  ],
+  "/blog/tags": [
+    "static/chunks/070dg~778a.yv.js"
+  ],
+  "/blog/tags/[tag]": [
+    "static/chunks/0w.o~webjf_tu.js"
+  ],
+  "/logo-lab": [
+    "static/chunks/0lkzwht.wassa.js"
+  ],
+  "/offline": [
+    "static/chunks/091u6148rd4ia.js"
+  ],
+  "/search": [
+    "static/chunks/123q8oymwavut.js"
+  ],
+  "/tags": [
+    "static/chunks/073-~phwttypu.js"
+  ],
+  "/tags/[tag]": [
+    "static/chunks/109a083.ir58i.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/_app",
+    "/_error",
+    "/about",
+    "/archive",
+    "/archive/[year]",
+    "/blog",
+    "/blog/about",
+    "/blog/archive",
+    "/blog/archive/[year]",
+    "/blog/articles",
+    "/blog/articles/[...slug]",
+    "/blog/posts",
+    "/blog/search",
+    "/blog/tags",
+    "/blog/tags/[tag]",
+    "/blog/[...slug]",
+    "/logo-lab",
+    "/offline",
+    "/search",
+    "/tags",
+    "/tags/[tag]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/0mrwuf-byrsqn.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/0ydd6~w3_fayi.js",
+  "static/chunks/0g.i9g4siywl4.js",
+  "static/chunks/0i.lj6jsn6e_1.js",
+  "static/chunks/0q77~okj5tq6y.js",
+  "static/chunks/08l14~ny3u6d6.css",
+  "static/chunks/turbopack-0ogl3.a8hjv_a.js"
+])
